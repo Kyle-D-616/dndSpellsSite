4c4090bf16494381a8a4e93f749d834f5a0eb18a
Kyle-D-616 <kyled616@gmail.com>
2025-03-22 03:01:25 -0400

    Initial commit, getting list of spells and basic info from home page

---
+++ getSpells.py
@@ -0,0 +1,24 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+session = HTMLSession()
+
+url = 'https://dnd5e.wikidot.com/spells'
+
+urlResponse = session.get(url)
+
+soup = BeautifulSoup(urlResponse.content, 'html.parser')
+table = soup.find('table')
+rows = soup.find_all('tr')
+
+for row in rows:
+    cells = row.find_all('td')
+    row_data = [cell.text.strip() for cell in cells]
+    print(row_data)
+
+
+#print(table)
+#products = r.html.find('')
+
+
