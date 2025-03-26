6cc4de6184c1c0af799a40d9384434be2dc1db53
Kyle-D-616 <kyled616@gmail.com>
2025-03-25 23:15:28 -0400

    getting all the urls for spells into urls

--- getSpells.py
+++ getSpells.py
@@ -9,16 +9,8 @@ url = 'https://dnd5e.wikidot.com/spells'
 urlResponse = session.get(url)
 
 soup = BeautifulSoup(urlResponse.content, 'html.parser')
-table = soup.find('table')
-rows = soup.find_all('tr')
-
-for row in rows:
-    cells = row.find_all('td')
-    row_data = [cell.text.strip() for cell in cells]
-    print(row_data)
-
-
-print(table)
-#products = r.html.find('')
-
-
+table = soup.find('div', {'class': 'yui-content'})
+urls = []
+for url in table.find_all('a'):
+    urls.append(url.get('href'))
+print(urls)
