af567a9838842f80ec617de4701216de7c23fc62
Kyle-D-616 <kyled616@gmail.com>
2025-03-28 00:53:56 -0400

    getting spell header title

--- getSpells.py
+++ getSpells.py
@@ -5,15 +5,22 @@ from bs4 import BeautifulSoup
 session = HTMLSession()
 
 baseUrl = 'https://dnd5e.wikidot.com/spells'
-spellUrl = f'https://dnd5e.wikidot.com{}'
+#spellUrl = f'https://dnd5e.wikidot.com{}'
 
 
 urlResponse = session.get(baseUrl)
-spellsUrlResponse = session.get(?)
 
 soup = BeautifulSoup(urlResponse.content, 'html.parser')
 table = soup.find('div', {'class': 'yui-content'})
 spellUrls = []
 for url in table.find_all('a'):
     spellUrls.append('https://dnd5e.wikidot.com'+url.get('href'))
-print(spellSoup)
+
+spells = []
+for spellUrl in spellUrls:
+    urlResponse = session.get(spellUrl)
+    soup = BeautifulSoup(urlResponse.content, 'html.parser')
+    spell = soup.find('div', {'class': 'page-title page-header'})
+    print(spell)
+    '''spellInfo = soup.find('div', {'class': 'page-content'})
+    spells.append({spell.get('span'): spellInfo.text} )'''
